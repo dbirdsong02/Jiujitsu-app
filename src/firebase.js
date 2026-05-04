fb32c24c1173c7fe1eb1c2e517c2a806ad8e80e1
Your Name <drew.birdsong@gmail.com>
2026-05-03 19:03:52 -0600

    add firebase auth and database

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAyTP1vWbWI713E0ise_opmy__7n_3sLSc",
+  authDomain: "bjj-journal-80c09.firebaseapp.com",
+  projectId: "bjj-journal-80c09",
+  storageBucket: "bjj-journal-80c09.firebasestorage.app",
+  messagingSenderId: "463750420966",
+  appId: "1:463750420966:web:7104984b4336b2ec465e27",
+  measurementId: "G-84VLR30P9N"
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
